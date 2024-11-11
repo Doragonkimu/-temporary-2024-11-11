a3bc21f5b97f264ac1f088a7b31fbb3f5adb4bb5
Doragonkimu <furumeyu918@gmail.com>
2024-11-11 01:55:38 +0000

    #1 uuid v4からuuid v7に変更

--- uuid.js
+++ uuid.js
@@ -1,5 +1,9 @@
-import { v4 } from "uuid";
-console.log(v4());
+import { v7 } from "uuid";
+console.log(v7());
+/**
+ * UUIDを偏こする
+ * @returns UUID v7
+ */
 
 export const sample = () => {
     return 'この関数は sample() です';
